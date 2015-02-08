3027ae76cc355365e0d3b828e99e527d321154d7
judy-eapen <judy-eapen@hotmail.com>
2015-02-07 20:29:40 -0500

    new line added: println

---
+++ HelloWorldSample/src/com/sample/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.sample.HelloWorld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		System.out.println("Hello Everyone");
+		System.out.println("I am new ");
+	}
+
+}
