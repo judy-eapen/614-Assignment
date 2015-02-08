25b63b09d869b532f483646674fc81f0bd7cc942
judy-eapen <judy-eapen@hotmail.com>
2015-02-07 20:40:06 -0500

    Update HelloWorld.java

--- HelloWorldSample/src/com/sample/HelloWorld/HelloWorld.java
+++ HelloWorldSample/src/com/sample/HelloWorld/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello Everyone");
 		System.out.println("I am new ");
+		System.out.println("I am second to new");
+	
 	}
 
 }
